5422500fbff0cdf17fedadd615ad63c8c442f765
SubhashreeP1212 <53390826+SubhashreeP1212@users.noreply.github.com>
2019-07-31 23:17:52 +0530

    Added merge project

---
+++ Merge/src/Merge1.java
@@ -0,0 +1,17 @@
+
+public class Merge1 {
+
+	public Merge1() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public static void main(String args[]) {
+		int a=10 , b=20 , c,d;
+		
+	System.out.println(c=a+b);	
+	
+	
+	}
+
+}
